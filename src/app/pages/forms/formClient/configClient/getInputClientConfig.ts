a1baf9cf6ab3cef9f9f432eff527cfe938075270
Fidele <jackie.fidele@laposte.net>
2024-11-07 12:26:32 +0100

    ✨ feat(formClient): add inputs navigation, update timeline

--- src/app/pages/forms/formClient/configClient/getInputClientConfig.ts
+++ src/app/pages/forms/formClient/configClient/getInputClientConfig.ts
@@ -1,4 +1,14 @@
-export const getInputClientConfig = [
+interface GetInputClientConfigType {
+  category: string
+  id: number
+  label: string
+  name: string
+  placeholder: string
+  required: boolean
+  type: string
+}
+
+export const getInputClientConfig: GetInputClientConfigType[][] = [
   [
     {
       category: 'compagny',
@@ -6,6 +16,8 @@ export const getInputClientConfig = [
       label: 'Nom Enteprise ou société',
       name: 'name',
       placeholder: "Entrez le nom de l'entrprise ou société",
+      required: false,
+      type: 'text',
     },
     {
       category: 'compagny',
@@ -13,6 +25,8 @@ export const getInputClientConfig = [
       label: 'SIRET',
       name: 'siret',
       placeholder: 'Entrez le SIRET',
+      required: false,
+      type: 'text',
     },
     {
       category: 'compagny',
@@ -20,6 +34,84 @@ export const getInputClientConfig = [
       label: 'Numéro de TVA',
       name: 'tva',
       placeholder: 'Entrez le numéro de TVA',
+      required: false,
+      type: 'text',
+    },
+  ],
+  [
+    {
+      category: 'compagny',
+      id: 3,
+      label: 'Adresse',
+      name: 'address',
+      placeholder: 'Entrez le nom de la rue',
+      required: false,
+      type: 'text',
+    },
+    {
+      category: 'compagny',
+      id: 4,
+      label: 'Ville',
+      name: 'city',
+      placeholder: 'Entrez la ville',
+      required: false,
+      type: 'text',
+    },
+    {
+      category: 'compagny',
+      id: 5,
+      label: 'Pays',
+      name: 'country',
+      placeholder: 'Entrez le pays',
+      required: false,
+      type: 'text',
+    },
+    {
+      category: 'compagny',
+      id: 6,
+      label: 'Code postal',
+      name: 'postal',
+      placeholder: 'Entrez le code postal',
+      required: false,
+      type: 'number',
+    },
+  ],
+  [
+    {
+      category: 'contact_client',
+      id: 7,
+      label: 'Nom du contact',
+      name: 'contact_name',
+      placeholder: 'Entrez le nom du contact',
+      required: false,
+      type: 'text',
+    },
+    {
+      category: 'contact_client',
+      id: 8,
+      label: 'Email',
+      name: 'email',
+      placeholder: 'Entrez l adresse email',
+      required: false,
+      type: 'email',
+    },
+    {
+      category: 'contact_client',
+      id: 9,
+      label: 'Numero de telephone',
+      name: 'phone',
+      placeholder: 'Entrez le numero de telephone',
+      required: false,
+      type: 'tel',
+    },
+    {
+      category: 'contact_client',
+      id: 10,
+      label: 'Commentaire',
+      name: 'note',
+      placeholder: 'Commentaire',
+      required: false,
+      type: 'text',
     },
   ],
 ]
--- src/app/pages/forms/formClient/formClient.tsx
+++ src/app/pages/forms/formClient/formClient.tsx
@@ -1,6 +1,25 @@
-import { type FC } from 'react'
+import { type FC, useEffect, useState } from 'react'
+
+import { getInputClientConfig } from './configClient/getInputClientConfig'
 
 const FormClient: FC = () => {
+  const [step, setStep] = useState(0)
+  const [currentStep, setCurrentStep] = useState(0)
+
+  const handleNextStep = () => {
+    if (currentStep === step - 1) return
+    setCurrentStep(currentStep + 1)
+  }
+  const handlePrevStep = () => {
+    if (currentStep === 0) return
+    setCurrentStep(currentStep - 1)
+  }
+
+  const getInput = getInputClientConfig
+  useEffect(() => {
+    setStep(getInput.length)
+  }, [getInput])
+
   return (
     <>
       {/*CONTAINER */}
@@ -17,7 +36,10 @@ const FormClient: FC = () => {
 
         {/*TIMELINE AREA */}
         <div className="border-stroke shadow-defaul dark:border-strokedark dark:bg-boxdark rounded-sm border bg-white">
-          <span>TIMELINE AREA</span>
+          <h3>TIMELINE AREA</h3>
+          <h1>
+            {currentStep + 1}/{step}
+          </h1>
         </div>
 
         {/*INPUT AREA CONTAINER*/}
@@ -26,27 +48,20 @@ const FormClient: FC = () => {
 
           {/*INPUT AREA 1*/}
           <div className="border-stroke shadow-defaul dark:border-strokedark dark:bg-boxdark rounded-sm border bg-blue-800"></div>
-          <span>INPUT AREA 1</span>
-          <span>Input 1</span>
-          <input
-            className="border-stroke focus:border-primary active:border-primary disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary w-1/3 rounded border-[1.5px] bg-transparent px-5 py-3 text-black outline-none transition disabled:cursor-default dark:text-white"
-            placeholder="test"
-            type="text"
-          />
+          <h2>-- INPUT AREA 1 --</h2>
 
-          <span>Input 2</span>
-          <input
-            className="border-stroke focus:border-primary active:border-primary disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary w-1/3 rounded border-[1.5px] bg-transparent px-5 py-3 text-black outline-none transition disabled:cursor-default dark:text-white"
-            placeholder="test"
-            type="text"
-          />
-
-          <span>Input 3</span>
-          <input
-            className="border-stroke focus:border-primary active:border-primary disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary w-1/3 rounded border-[1.5px] bg-transparent px-5 py-3 text-black outline-none transition disabled:cursor-default dark:text-white"
-            placeholder="test"
-            type="text"
-          />
+          {getInput[currentStep].map((inputs) => {
+            return (
+              <div key={inputs.id}>
+                <span>{inputs.label}</span>
+                <input
+                  className="border-stroke focus:border-primary active:border-primary disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary w-1/3 rounded border-[1.5px] bg-transparent px-5 py-3 text-black outline-none transition disabled:cursor-default dark:text-white"
+                  placeholder={inputs.placeholder}
+                  type={inputs.type}
+                />
+              </div>
+            )
+          })}
 
           {/*INPUT AREA 2*/}
           <div className="border-stroke shadow-defaul dark:border-strokedark dark:bg-boxdark rounded-sm border bg-blue-800"></div>
@@ -56,6 +71,12 @@ const FormClient: FC = () => {
         {/*STEP AREA */}
         <div className="border-stroke shadow-defaul dark:border-strokedark dark:bg-boxdark rounded-sm border bg-red-500">
           <span>STEP AREA</span>
+          <div>
+            <button onClick={handlePrevStep}>Précédent</button>
+            <br />
+            <span> </span>
+            <button onClick={handleNextStep}>Suivant</button>
+          </div>
         </div>
       </div>
     </>
