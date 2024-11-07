995cbf349095a49f7909ec5c2d16f0f107037e10
Fidele <jackie.fidele@laposte.net>
2024-11-07 09:39:12 +0100

    ✨ feat(formClient):create formClient frame and configClient

--- src/app/pages/dashboard/index.tsx
+++ src/app/pages/dashboard/index.tsx
@@ -1,11 +1,17 @@
 import { type FC } from 'react'
 
+import FormClient from '../forms/formClient/formClient'
+
 const Dashboard: FC = () => {
   return (
     <>
       <div>(PAGE CONTENT) - DASHBOARD</div>
       <div className="flex items-center justify-center bg-green-400/25">
         <label htmlFor="checkbox-toggle-sidebar">CHECK</label>
+      </div>
+      <div>
+        <h1>FORMULAIRE CLIENT</h1>
+        <FormClient />
       </div>
     </>
   )
