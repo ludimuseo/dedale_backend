9208d6a8a2457f05dc6448c24373642d51f52119
Fidele <jackie.fidele@laposte.net>
2024-11-10 23:58:44 +0100

    feat(input area):Add translate prop to ClientType

--- src/app/pages/forms/formClient/formClient.tsx
+++ src/app/pages/forms/formClient/formClient.tsx
@@ -103,12 +103,7 @@ const FormClient: FC = () => {
   return (
     <>
       {/*CONTAINER */}
-      <div className="grid grid-cols-1 gap-1 p-7 sm:grid-cols-1">
-        {/*SELECTION AREA */}
-        <div className="border-stroke shadow-defaul dark:border-strokedark dark:bg-boxdark rounded-sm border bg-white">
-          <span>SELECTION AREA</span>
-        </div>
-
+      <div className="grid grid-cols-1 gap-1 p-10 sm:grid-cols-1">
         {/*NAVIGATION AREA */}
         <div className="border-stroke shadow-defaul dark:border-strokedark dark:bg-boxdark rounded-sm border bg-white">
           <span>NAVIGATION AREA</span>
@@ -222,14 +217,13 @@ const FormClient: FC = () => {
         </div>
 
         {/*INPUT AREA CONTAINER*/}
-        <div className="border-stroke shadow-defaul dark:border-strokedark dark:bg-boxdark dark:border-strokedark dark:bg-boxdark flex flex-row rounded-sm border bg-white p-5">
+        <div className="border-stroke shadow-defaul dark:border-strokedark dark:bg-boxdark dark:border-strokedark dark:bg-boxdark flex flex-row justify-center rounded-sm border bg-white p-5">
           {/*INPUT AREA 1*/}
           {!message.info ? (
             <form
               onSubmit={() => handleSubmit}
-              className="border-stroke shadow-defaul dark:border-strokedark dark:bg-boxdark mt-5 flex w-1/2 flex-col rounded-sm border p-2">
-              <span>-- INPUT AREA 1 --</span>
-
+              className="border-stroke shadow-defaul dark:border-strokedark dark:bg-boxdark mt-5 flex w-1/2 flex-col rounded-sm p-2">
+              {/* <span>-- INPUT AREA 1 --</span> */}
               {getInput[currentStep].map(
                 ({ id, section, rows, type, name, label, placeholder }) => {
                   console.log('section', section, 'name', name)
@@ -241,7 +235,7 @@ const FormClient: FC = () => {
                           key={id}
                           id={id}
                           name={name}
-                          className="border-stroke focus:border-primary active:border-primary disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary w-2/3 rounded border-[1.5px] bg-transparent px-5 py-3 text-black outline-none transition disabled:cursor-default dark:text-white"
+                          className="border-stroke disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary w-4/6 rounded border-[1.5px] bg-transparent px-5 py-3 text-black outline-none transition focus:border-blue-800 active:border-blue-400 disabled:cursor-default dark:text-white"
                           placeholder={placeholder}
                           rows={rows}
                           value={
@@ -268,7 +262,7 @@ const FormClient: FC = () => {
                           key={id}
                           id={id}
                           name={name}
-                          className="border-stroke focus:border-primary active:border-primary disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary w-2/3 rounded border-[1.5px] bg-transparent px-5 py-3 text-black outline-none transition disabled:cursor-default dark:text-white"
+                          className="border-stroke disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary w-4/6 rounded border-[1.5px] bg-transparent px-5 py-3 text-black outline-none transition focus:border-blue-800 active:border-blue-400 disabled:cursor-default dark:text-white"
                           placeholder={placeholder}
                           type={type}
                           value={
@@ -293,15 +287,26 @@ const FormClient: FC = () => {
             </form>
           ) : (
             <>
-              <div className="border-stroke shadow-defaul dark:border-strokedark dark:bg-boxdark mt-5 flex w-1/2 flex-col items-center rounded-sm border p-2">
+              <div className="border-stroke shadow-defaul dark:border-strokedark dark:bg-boxdark mt-5 flex w-1/2 flex-col items-center rounded-sm p-2">
                 <h1> ✅ {message.info}</h1>
                 <img width="30%" src={successImage} alt="Success Minos" />
               </div>
             </>
           )}
-          <div className="border-stroke shadow-defaul dark:border-strokedark dark:bg-boxdark mt-5 w-1/2 rounded-sm border bg-yellow-300 p-2">
-            <span>-- TRANSLATION ZONE --</span>
-          </div>
+          {
+            //translate eonz s'affiche si il y a une traduction a executer
+            getInput[currentStep].map(({ translate }, index) => {
+              if (translate) {
+                return (
+                  <div
+                    key={index}
+                    className="border-stroke shadow-defaul dark:border-strokedark dark:bg-boxdark mt-5 w-1/2 rounded-sm border bg-sky-100 p-2">
+                    <span>-- TRANSLATION ZONE --</span>
+                  </div>
+                )
+              }
+            })
+          }
         </div>
 
         {/*STEP AREA */}
