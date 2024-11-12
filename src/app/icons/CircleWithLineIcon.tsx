e9d5f7817f37d181c5c21b78f2d8e800531e89c1
Fidele <jackie.fidele@laposte.net>
2024-11-12 12:00:03 +0100

    🌈 style(circle):Modif css

--- src/app/icons/CircleWithLineIcon.tsx
+++ src/app/icons/CircleWithLineIcon.tsx
@@ -15,7 +15,7 @@ export const CircleWithLineIcon = ({
   return (
     <svg height="100" width="200" key={index} {...props}>
       <circle
-        cx="50"
+        cx="70"
         cy="50"
         r="25"
         stroke="#4AD87E"
@@ -23,7 +23,7 @@ export const CircleWithLineIcon = ({
         fill="#4AD87E"
       />
       <path
-        d="M35 50 L45 60 L65 40"
+        d="M55 50 L65 60 L85 40"
         fill="none"
         stroke="white"
         strokeWidth="3"
