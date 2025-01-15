16188ff7917bfaa3b260435ca1ce63bfa78d268b
Camille CYL-B <benhammadcamille@hotmail.fr>
2025-01-15 16:37:52 +0100

    :sparkles:feat(mainContent): added animation when mainContent appears in the DOM

--- src/app/components/talos/main/mainContent.tsx
+++ src/app/components/talos/main/mainContent.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import React, { ReactElement } from 'react'
 
 import { GameType, JourneyType, PieceType, PlaceType, StepType } from '@/types'
@@ -15,14 +16,18 @@ interface MainContentProps {
 
 const MainContent: React.FC<MainContentProps> = ({ sentencesData }) => {
   return (
-    <div className="mt-4 h-auto rounded-md border-2 border-black bg-white p-4 shadow-2xl">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3, ease: 'linear' }}
+      className="mt-4 h-auto rounded-md border-2 border-black bg-white p-4 shadow-2xl">
       {sentencesData && (
         <>
           <h2 className="mb-4 text-2xl font-bold text-primary">Je lis :</h2>
           <div className="space-y-4">{sentencesData}</div>
         </>
       )}
-    </div>
+    </motion.div>
   )
 }
 
