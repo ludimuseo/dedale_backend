2f2588923e8731911f0c47eb94d078ad46bb7741
echo-vic <victorleonard@hotmail.fr>
2025-01-16 21:32:10 +0100

    :art:style(BOD18): new pagination

--- src/app/components/ui/pagination.tsx
+++ src/app/components/ui/pagination.tsx
@@ -2,85 +2,81 @@ import '@/assets/styles/root.scss'
 
 import React from 'react'
 
-const Pagination: React.FC = () => {
+interface PaginationProps {
+  onPreviousPageEvent: () => void
+  onNextPageEvent: () => void
+  disablePrevious: boolean
+  disableNext: boolean
+  currentPage: number
+}
+
+const Pagination: React.FC<PaginationProps> = ({
+  onPreviousPageEvent,
+  onNextPageEvent,
+  disablePrevious,
+  disableNext,
+  currentPage,
+}) => {
+  const handlePreviousClick = () => {
+    if (!disablePrevious) {
+      onPreviousPageEvent()
+    }
+  }
+
+  const handleNextClick = () => {
+    if (!disableNext) {
+      onNextPageEvent()
+    }
+  }
+
   return (
     <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-      <div>
-        <p className="text-sm text-gray-700">
-          Affichage de <span className="font-medium">1</span> à{' '}
-          <span className="font-medium">10</span> sur{' '}
-          <span className="font-medium">97</span> résultats
-        </p>
-      </div>
-      <div>
-        <nav
-          aria-label="Pagination"
-          className="isolate inline-flex -space-x-px rounded-md shadow-sm">
-          <a
-            href="#"
-            className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+      <div className="flex flex-1 justify-between sm:justify-end">
+        <span className="isolate inline-flex rounded-md shadow-sm">
+          <button
+            disabled={disablePrevious}
+            type="button"
+            onClick={handlePreviousClick}
+            className={`${disablePrevious ? 'cursor-not-allowed opacity-40' : 'hover:bg-gray-50'} relative inline-flex items-center rounded-l-md bg-white px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 focus:z-10`}>
             <span className="sr-only">Previous</span>
             <svg
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
+              xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="w-4">
-              <polyline points="15 18 9 12 15 6"></polyline>
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5 8.25 12l7.5-7.5"
+              />
             </svg>
-          </a>
-          <a
-            href="#"
-            aria-current="page"
-            className="focus-visible:outline-primary-600 relative z-10 inline-flex items-center bg-primary px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
-            1
-          </a>
-          <a
-            href="#"
-            className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-            2
-          </a>
-          <a
-            href="#"
-            className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">
-            3
-          </a>
-          <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
-            ...
-          </span>
-          <a
-            href="#"
-            className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">
-            8
-          </a>
-          <a
-            href="#"
-            className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-            9
-          </a>
-          <a
-            href="#"
-            className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-            10
-          </a>
-          <a
-            href="#"
-            className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+          </button>
+          <div className="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300">
+            {currentPage + 1}
+          </div>
+          <button
+            type="button"
+            disabled={disableNext}
+            onClick={handleNextClick}
+            className={`${disableNext ? 'cursor-not-allowed opacity-40' : 'hover:bg-gray-50'}relative -ml-px inline-flex items-center rounded-r-md bg-white px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10`}>
             <span className="sr-only">Next</span>
             <svg
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
+              xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="w-4">
-              <polyline points="9 18 15 12 9 6"></polyline>
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m8.25 4.5 7.5 7.5-7.5 7.5"
+              />
             </svg>
-          </a>
-        </nav>
+          </button>
+        </span>
       </div>
     </div>
   )
