292c43ce90e59b9e6c1237bc4208d1d1b6fffaa5
echo-vic <victorleonard@hotmail.fr>
2025-01-16 21:33:54 +0100

    :sparkles:feat(BOD18): fetch users list from firebase

--- src/app/components/ui/dataTable.tsx
+++ src/app/components/ui/dataTable.tsx
@@ -22,6 +22,11 @@ interface DataTableProps {
   data: Record<string, unknown>[] | null
   isLoading: boolean
   actions?: Action[]
+  previousPage?: () => void
+  nextPage?: () => void
+  disablePrevious: boolean
+  disableNext: boolean
+  currentPage: number
 }
 
 const DataTable: React.FC<DataTableProps> = ({
@@ -29,6 +34,11 @@ const DataTable: React.FC<DataTableProps> = ({
   data,
   isLoading,
   actions,
+  nextPage,
+  previousPage,
+  disablePrevious,
+  disableNext,
+  currentPage,
 }) => {
   if (!columns || !data) {
     return <div>Invalid data or columns</div>
@@ -138,80 +148,19 @@ const DataTable: React.FC<DataTableProps> = ({
                 </td>
               </tr>
             )}
-            {!isLoading ? (
-              data.map((row, rowIndex) => (
-                <tr key={row.id || rowIndex}>
-                  {columns.map((col, colIndex) => (
-                    <td key={colIndex}>{row[col.accessor]}</td>
-                  ))}
-                  {actions && actions.length > 0 && (
-                    <td className="sticky right-0 flex min-w-40 border-l-2 border-gray-100 bg-white">
-                      <div className="m-auto">
-                        {actions.map((action, actionIndex) => (
-                          <button
-                            key={actionIndex}
-                            onClick={() => {
-                              action.onClick(row.id)
-                            }}
-                            className="focus:shadow-outline group mr-2 inline-flex h-8 w-8 items-center justify-center rounded-full bg-white text-gray-700 transition-colors duration-150 hover:bg-gray-200">
-                            {action.type === 'edit' && (
-                              <svg
-                                className="h-6 w-6 text-gray-800 dark:text-white"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                fill="none"
-                                viewBox="0 0 24 24">
-                                <path
-                                  stroke="currentColor"
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  strokeWidth="2"
-                                  d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"
-                                />
-                              </svg>
-                            )}
-                            {action.type === 'location' && <PlaceIcon />}
-                            {action.type === 'delete' && (
-                              <svg
-                                className="h-5 w-5 text-gray-800 group-hover:text-red-500 dark:text-white"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                fill="none"
-                                viewBox="0 0 24 24">
-                                <path
-                                  stroke="currentColor"
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  strokeWidth="2"
-                                  d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
-                                />
-                              </svg>
-                            )}
-                          </button>
-                        ))}
-                      </div>
-                    </td>
-                  )}
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan={columns.length}>
-                  <span className="loading loading-ring loading-lg m-auto flex"></span>
-                </td>
-              </tr>
-            )}
           </tbody>
         </table>
       </div>
       <div
         className="absolute"
-        style={{ bottom: '-50px', left: '0', right: '0', width: '99%' }}>
-        <Pagination />
+        style={{ bottom: '-60px', left: '0', right: '0', width: '100%' }}>
+        <Pagination
+          onPreviousPageEvent={previousPage}
+          onNextPageEvent={nextPage}
+          disablePrevious={disablePrevious}
+          disableNext={disableNext}
+          currentPage={currentPage}
+        />
       </div>
     </div>
   )
--- src/app/pages/users/index.tsx
+++ src/app/pages/users/index.tsx
@@ -1,27 +1,107 @@
-import { type FC } from 'react'
+import {
+  collection,
+  DocumentSnapshot,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+} from 'firebase/firestore'
+import { FC, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router'
 
 import DataTable from '@/app/components/ui/dataTable'
-import { useFetch } from '@/app/hooks/useFetch'
-
-interface User {
-  username: string
-  name: string
-  email: string
-  phone: string
-}
+import { db } from '@/firebase/firebase'
+import { ClientType } from '@/types'
 
 const Users: FC = () => {
-  const { data, isLoading, error } = useFetch<User[]>(
-    'https://jsonplaceholder.typicode.com/users'
-  )
   const navigate = useNavigate()
+  const [users, setUsers] = useState<ClientType[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(0)
 
-  if (error) return <p>Erreur : {error}</p>
+  const [pages, setPages] = useState<DocumentSnapshot[]>([])
+
+  const pageSize = 10 // Number of items per page
+
+  const formatData = (data: ClientType[]) => {
+    return data.map((el) => ({
+      id: el.id,
+      name: el.company.name || 'Nom non défini',
+      contact: el.contact.name || 'Non spécifié',
+      email: el.contact.email || 'Non spécifié',
+      phone: el.contact.tel || 'Non spécifié',
+    }))
+  }
+
+  const fetchUsers = async (pageIndex: number) => {
+    setIsLoading(true)
+
+    try {
+      let usersQuery
+
+      // Define the query for the given page
+      if (pageIndex === 0) {
+        // Initial page
+        usersQuery = query(
+          collection(db, 'clients'),
+          orderBy('client.company.name'),
+          limit(pageSize)
+        )
+      } else {
+        // Other pages
+        const startPoint = pages[pageIndex - 1]
+        usersQuery = query(
+          collection(db, 'clients'),
+          orderBy('client.company.name'),
+          startAfter(startPoint),
+          limit(pageSize)
+        )
+      }
+
+      const querySnapshot = await getDocs(usersQuery)
+
+      // Save the last document for pagination
+      if (querySnapshot.docs.length > 0) {
+        const newPages = [...pages]
+        newPages[pageIndex] = querySnapshot.docs[querySnapshot.docs.length - 1]
+        setPages(newPages)
+      }
+
+      // Format and set user data
+      const usersData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        collection: 'clients',
+        ...(doc.data().client as ClientType),
+      }))
+      setUsers(formatData(usersData))
+    } catch (error) {
+      console.error('Error fetching clients:', error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const nextPage = () => {
+    setCurrentPage((prev) => prev + 1)
+    void fetchUsers(currentPage + 1)
+  }
+
+  const previousPage = () => {
+    if (currentPage > 0) {
+      const prevIndex = currentPage - 1
+      setCurrentPage(prevIndex)
+      void fetchUsers(prevIndex)
+    }
+  }
+
+  useEffect(() => {
+    void fetchUsers(0)
+  }, [])
 
   const columns = [
-    { header: 'Entreprise ou société', accessor: 'username' },
-    { header: 'Contact', accessor: 'name' },
+    { header: 'Entreprise ou société', accessor: 'name' },
+    { header: 'Contact', accessor: 'contact' },
     { header: 'Email', accessor: 'email' },
     { header: 'Téléphone', accessor: 'phone' },
   ]
@@ -40,29 +120,32 @@ const Users: FC = () => {
     {
       type: 'location',
       onClick: (id: string) => {
-        console.log(`location ${id}`)
+        console.log(`Location: ${id}`)
       },
     },
     {
       type: 'delete',
       onClick: (id: string) => {
-        console.log(`Delete ${id}`)
+        console.log(`Delete: ${id}`)
       },
     },
   ]
 
   return (
-    <>
-      <div>
-        <h2 className="mb-5">Users</h2>
-        <DataTable
-          columns={columns}
-          data={data}
-          actions={actions}
-          isLoading={isLoading}
-        />
-      </div>
-    </>
+    <div>
+      <h2 className="mb-5">Users</h2>
+      <DataTable
+        columns={columns}
+        data={users}
+        actions={actions}
+        isLoading={isLoading}
+        nextPage={nextPage}
+        previousPage={previousPage}
+        disableNext={users.length < pageSize} // Disable "Next" if fewer items than pageSize
+        disablePrevious={currentPage === 0}
+        currentPage={currentPage} // Disable "Previous" on the first page
+      />
+    </div>
   )
 }
 
