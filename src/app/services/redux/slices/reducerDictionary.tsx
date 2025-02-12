69a604e3c80282604ea8ae40e7aff33b56a03f88
Camille CYL-B <benhammadcamille@hotmail.fr>
2025-02-12 20:14:00 +0100

    :bug:fix(reducerDictionary): remove custom fetch hook, add open state

--- src/app/services/redux/slices/reducerDictionary.tsx
+++ src/app/services/redux/slices/reducerDictionary.tsx
@@ -1,30 +1,31 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
-import { useFetch } from '@/app/hooks/useFetch'
+// import { useFetch } from '@/app/hooks/useFetch'
 
-interface DictionaryState {
+export interface StateDictionnary {
   selectedWord: string | null
   definition: string | null
-  position: { x: number; y: number } | null
+  // position: { x: number; y: number } | null
+  open: boolean
   loading: boolean
   error: string | null
 }
 
 interface SetWordActionPayload {
   word: string | null
-  x: number
-  y: number
+  // x: number
+  // y: number
 }
 
-// Async thunk for fetching definitions
+// Async thunk for fetching definitions from Wiki
 export const fetchDefinition = createAsyncThunk(
   'dictionary/fetchDefinition',
-  (word: string) => {
-    const { data } = useFetch(
+  async (word: string) => {
+    const response = await fetch(
       `https://fr.wiktionary.org/w/api.php?action=query&format=json&prop=extracts&titles=${word}&origin=*`
     )
 
-    const fetchData = data as {
+    const fetchData = (await response.json()) as {
       query: {
         pages: Record<
           string,
@@ -39,14 +40,22 @@ export const fetchDefinition = createAsyncThunk(
     if (page.extract) {
       const parser = new DOMParser()
       const doc = parser.parseFromString(page.extract, 'text/html')
-      const paragraphs = doc.querySelectorAll('p')
+      const paragraphs = doc.querySelectorAll('ol li')
+      const text = paragraphs[0].textContent?.trim()
+      const endOfSentence = '.'
+      const indexEnd = Number(text?.indexOf(endOfSentence)) + 1
+      const sentence = text?.slice(0, indexEnd)
 
-      for (const p of paragraphs) {
-        const text = p.textContent?.trim()
-        if (text && text.length > 20) {
-          return text
-        }
+      if (sentence) {
+        return sentence
       }
+
+      // for (const p of paragraphs) {
+      //   const text = p.textContent?.trim()
+      //   if (text && text.length > 20) {
+      //     return text
+      //   }
+      // }
     }
     return 'Définition introuvable.'
   }
@@ -58,21 +67,25 @@ const sliceDictionary = createSlice({
     selectedWord: null,
     definition: null,
     position: null,
+    open: false,
     loading: false,
     error: null,
-  } as DictionaryState,
+  } as StateDictionnary,
   reducers: {
     setWord: (state, action: { payload: SetWordActionPayload }) => {
       state.selectedWord = action.payload.word
-      state.position = { x: action.payload.x, y: action.payload.y }
+      // state.position = { x: action.payload.x, y: action.payload.y }
       state.definition = null // Reset definition while loading
       state.error = null
     },
     clearWord: (state) => {
       state.selectedWord = null
       state.definition = null
-      state.position = null
+      // state.position = null
       state.error = null
+    },
+    toggleSearch: (state) => {
+      state.open = !state.open
     },
   },
   extraReducers: (builder) => {
@@ -92,5 +105,5 @@ const sliceDictionary = createSlice({
   },
 })
 
-export const { setWord, clearWord } = sliceDictionary.actions
+export const { setWord, clearWord, toggleSearch } = sliceDictionary.actions
 export default sliceDictionary.reducer
