d663159f85f688a8552e7f0d277063b8c5a24f11
Camille CYL-B <benhammadcamille@hotmail.fr>
2025-02-12 20:16:52 +0100

    :sparkles:feat(textWrapper): add wrapper to include tooltip

---
+++ src/app/components/talos/textWrapper.tsx
@@ -0,0 +1,42 @@
+import { useAppDispatch } from '@hook/index'
+import { UnknownAction } from '@reduxjs/toolkit'
+import type { RootState } from '@service/redux'
+import {
+  clearWord,
+  fetchDefinition,
+  setWord,
+} from '@service/redux/slices/reducerDictionary'
+import { ThunkDispatch } from 'redux-thunk'
+
+import { DefinitionTooltip } from './definitionTooltip'
+
+interface TextWrapperProps {
+  text: string
+}
+
+export const TextWrapper: React.FC<TextWrapperProps> = ({ text }) => {
+  const dispatch: ThunkDispatch<RootState, void, UnknownAction> =
+    useAppDispatch()
+
+  const handleDoubleClick = () => {
+    dispatch(clearWord())
+    const selection = window.getSelection()
+    if (selection) {
+      const word = selection.toString().trim()
+      if (word) {
+        // const x = event.clientX
+        // const y = event.clientY
+        dispatch(setWord({ word }))
+        void dispatch(fetchDefinition(word)) // Fetch definition
+      }
+    }
+  }
+
+  return (
+    <DefinitionTooltip>
+      <p className="text-center text-xl font-bold" onClick={handleDoubleClick}>
+        {text}
+      </p>
+    </DefinitionTooltip>
+  )
+}
