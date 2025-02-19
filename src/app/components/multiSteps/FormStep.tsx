d8b274399be6e78e00b124de86e6a824ac290464
GJMCharles <91600327+GJMCharles@users.noreply.github.com>
2025-02-19 16:22:25 +0100

    :pencil:docs(miscellaneous): first step

---
+++ src/app/components/multiSteps/FormStep.tsx
@@ -0,0 +1,13 @@
+import { type FC } from 'react'
+
+const FormStep: FC = () => {
+  return (
+    <>
+      <div>
+        <h1>JE SUIS ROOTY!</h1>
+      </div>
+    </>
+  )
+}
+
+export { FormStep }
---
+++ src/app/pages/test/index.tsx
@@ -0,0 +1,15 @@
+import { FormStep } from '@component'
+import { type FC } from 'react'
+
+const Test: FC = () => {
+  return (
+    <>
+      <div>
+        <h1>JE SUIS GROOT!</h1>
+        <FormStep></FormStep>
+      </div>
+    </>
+  )
+}
+
+export { Test }
