9b21d50f76653d30f835a3371e47654430752015
echo-vic <victorleonard@hotmail.fr>
2025-03-11 14:46:25 +0100

    :sparkles:feat(BOD3): formEdit folder

---
+++ src/app/components/ui/FormEdit/FormCard.tsx
@@ -0,0 +1,20 @@
+import { ComponentPropsWithoutRef, forwardRef } from 'react'
+
+const FromCard = forwardRef<HTMLDivElement, ComponentPropsWithoutRef<'div'>>(
+  ({ children, title, ...props }, ref) => {
+    return (
+      <div className="p-1">
+        <div {...props} ref={ref} className="card mx-0 bg-base-100 shadow-md">
+          <div className="card-body">
+            <h2 className="card-title">{title}</h2>
+            <div className="grid grid-cols-1 gap-x-6 gap-y-2 sm:grid-cols-6">
+              {children}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+)
+
+export { FromCard }
