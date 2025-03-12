5fa3feec4dac73fe7d9596db11e9f9f556a0ad4f
Fidele <jackie.fidele@laposte.net>
2025-03-13 00:25:21 +0100

    :bug:fix(Layout): fix bug import file

--- src/app/layouts/LayoutDefault.tsx
+++ src/app/layouts/LayoutDefault.tsx
@@ -2,7 +2,8 @@
 import type { FC } from 'react'
 import { Outlet } from 'react-router'
 
-import CoreHeader from '@/app/layouts/core/CoreHeader'
+import CoreHeader from './core/CoreHeader'
+
 // import CoreSidebar from '@/app/layouts/core/coreSidebar'
 
 const LayoutDefault: FC = () => {
