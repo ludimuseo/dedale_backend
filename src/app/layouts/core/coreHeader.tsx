a32ae8d58844e21321f632262d245519d7fece0d
Fidele <jackie.fidele@laposte.net>
2025-03-15 15:25:45 +0100

    fix(merge): merge feature-4

--- src/app/layouts/core/coreHeader.tsx
+++ src/app/layouts/core/coreHeader.tsx
@@ -3,28 +3,28 @@ import { type FC, ReactElement } from 'react'
 import { NavLink } from 'react-router'
 
 interface HeaderProps {
-  toggleMenu: ReactElement<HTMLElement> | null
+    toggleMenu: ReactElement<HTMLElement> | null
 }
 
 const CoreHeader: FC<HeaderProps> = ({ toggleMenu }) => {
-  return (
-    <>
-      <header className="navbar min-h-24 p-4">
-        <div className="navbar-start gap-x-3">
-          {toggleMenu}
-          <div className="logo-container">
-            <NavLink to={{ pathname: '/' }}>
-              <LogoDedale width={140} />
-            </NavLink>
-          </div>
-        </div>
-        <div className="navbar-center"></div>
-        <div className="navbar-end">
-          <BlockUserMenu />
-        </div>
-      </header>
-    </>
-  )
+    return (
+        <>
+            <header className="navbar min-h-24 p-4">
+                <div className="navbar-start gap-x-3">
+                    {toggleMenu}
+                    <div className="logo-container">
+                        <NavLink to={{ pathname: '/' }}>
+                            <LogoDedale width={140} />
+                        </NavLink>
+                    </div>
+                </div>
+                <div className="navbar-center"></div>
+                <div className="navbar-end">
+                    <BlockUserMenu />
+                </div>
+            </header>
+        </>
+    )
 }
 
 export default CoreHeader
