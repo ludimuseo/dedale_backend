7b685e5cfd5a8310e59457ed912af9c93e550426
Camille CYL-B <benhammadcamille@hotmail.fr>
2025-04-06 21:56:18 +0200

    :bug:fix(changeFontSizeBar): adjusted style on hover and on smaller screens

--- src/app/components/talos/ChangeFontSizeBar.tsx
+++ src/app/components/talos/ChangeFontSizeBar.tsx
@@ -9,19 +9,21 @@ const ChangeFontSizeBar: FC<ChangeFontSizeBarProps> = ({
   handleDecreaseFontSize,
   handleIncreaseFontSize,
 }) => {
+  const buttonStyle =
+    'btn-square rounded-md text-[#0a184d] bg-white hover:text-white hover:bg-[#0a184d] focus:ring focus:ring-blue-300 focus:ring-opacity-50'
   return (
-    <div className="flex flex-col justify-start gap-1">
+    <div className="flex justify-start gap-1 lg:flex-col">
       <button
-        className="btn-square bg-white"
+        className={buttonStyle}
         onClick={handleDecreaseFontSize}
         aria-label="Diminuer la taille de la police">
-        <p className="text-base text-[#0a184d]">A</p>
+        <p className="text-base">A</p>
       </button>
       <button
-        className="btn-square bg-white"
+        className={buttonStyle}
         onClick={handleIncreaseFontSize}
         aria-label="Augmenter la taille de la police">
-        <p className="text-3xl text-[#0a184d]">A</p>
+        <p className="text-3xl">A</p>
       </button>
     </div>
   )
